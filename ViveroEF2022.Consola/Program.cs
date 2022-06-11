b50033691855ce2522fd3e0b7ad9e2ba0bb6442c
ciminoce <ciminoce@yahoo.com.ar>
2022-06-11 16:53:19 -0300

    Temas del 10-06-2022

--- ViveroEF2022.Consola/Program.cs
+++ ViveroEF2022.Consola/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.InteropServices;
 using ViveroEF2022.Context;
 using ViveroEF2022.Entities;
 
@@ -30,6 +31,15 @@ namespace ViveroEF2022.Consola
                 Console.WriteLine("10-Mostrar Paginado");
                 Console.WriteLine("11-Agregar un tipo de planta");
                 Console.WriteLine("12-Agregar una nueva Planta");
+                Console.WriteLine("13-Agrupar por tipo");
+                Console.WriteLine("14-Datos estadísticos por tipo de Envase");
+                Console.WriteLine("15-Estadisticos mayores a 30 registros");
+                Console.WriteLine("16-Primeros 10 mayores a un valor");
+                Console.WriteLine("17-Todas menores a un valor random");
+                Console.WriteLine("18-Alguna igual a un valor random");
+                Console.WriteLine("19-Inferiores al precio promedio");
+                Console.WriteLine("20-Mostrar mientras precio inferior a 100");
+                Console.WriteLine("21-Saltar los precios menores a 100");
                 var opcion = int.Parse(Console.ReadLine());
                 switch (opcion)
                 {
@@ -72,12 +82,225 @@ namespace ViveroEF2022.Consola
                     case 12:
                         AgregarNuevaPlanta();
                         break;
-                        
-
+                    case 13:
+                        AgruparPorTipo();
+                        break;
+                    case 14:
+                        EstadisticaPorTipoEnvase();
+                        break;
+                    case 15:
+                        EstadisticasPorTipoEnvaseMasDe30();
+                        break;
+                    case 16:
+                        Primeros10Mayores();
+                        break;
+                    case 17:
+                        TodasMenoresARandom();
+                        break;
+                    case 18:
+                        AlgunaIgualARandom();
+                        break;
+                    case 19:
+                        InferioresAlPrecioPromedio();
+                        break;
+                    case 20:
+                        MostrarMientrasInferior100();
+                        break;
+                    case 21:
+                        SaltarMientrasInferior100();
+                        break;
                 }
             } while (continua);
 
 
+        }
+
+        private static void SaltarMientrasInferior100()
+        {
+            using (var context = new ViveroDbContext())
+            {
+                var query = context.Plantas.OrderBy(p => p.PrecioVenta)
+                    .ToList();
+                var plantas = query.SkipWhile(p => p.PrecioVenta < 100)
+                .ToList();
+                foreach (var planta in plantas)
+                {
+                    Console.WriteLine($"{planta.Descripcion} {planta.PrecioVenta}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void MostrarMientrasInferior100()
+        {
+            using (var context = new ViveroDbContext())
+            {
+                var query = context.Plantas.ToList();
+                var menores = query.TakeWhile(p => p.PrecioVenta < 300)
+                    .ToList();
+                Console.WriteLine($"{menores.Count()}");
+                foreach (var planta in menores)
+                {
+                    Console.WriteLine($"{planta.Descripcion} {planta.PrecioVenta}");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void InferioresAlPrecioPromedio()
+        {
+            using (var context = new ViveroDbContext())
+            {
+                var precioPromedio = context.Plantas.Average(p => p.PrecioVenta);
+                Console.WriteLine($"Precio promedio= {precioPromedio}");
+                var query = context.Plantas.Where(p => p.PrecioVenta < precioPromedio).ToList();
+                foreach (var planta in query)
+                {
+                    Console.WriteLine($"{planta.Descripcion} {planta.PrecioVenta}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void AlgunaIgualARandom()
+        {
+            var r = new Random();
+            var valor = r.Next(50, 61);
+            using (var context = new ViveroDbContext())
+            {
+                var query = context.Plantas.Any(p => p.PrecioVenta == valor);
+                if (query)
+                {
+                    var plantas = context.Plantas
+                        .Where(p => p.PrecioVenta == valor)
+                        .ToList();
+                    foreach (var planta in plantas)
+                    {
+                        Console.WriteLine($"{planta.Descripcion} {planta.PrecioVenta}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"No hay ninguna planta con el precio igual a {valor}");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static void TodasMenoresARandom()
+        {
+            var r = new Random();
+            var valor = r.Next(250, 3000);
+            using (var context = new ViveroDbContext())
+            {
+                var query = context.Plantas.All(p => p.PrecioVenta < valor);
+                if (query)
+                {
+                    Console.WriteLine($"Todas menores a {valor}");
+                }
+                else
+                {
+                    Console.WriteLine($"No todas son menores a {valor}");
+                    var plantas = context.Plantas.Where(p => p.PrecioVenta >= valor).ToList();
+                    foreach (var planta in plantas)
+                    {
+                        Console.WriteLine($"{planta.Descripcion} {planta.PrecioVenta}");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Primeros10Mayores()
+        {
+            Console.Write("Ingrese un precio para comparar:");
+            var precio = decimal.Parse(Console.ReadLine());
+            using (var context = new ViveroDbContext())
+            {
+                var query = context.Plantas.Where(p => p.PrecioVenta > precio)
+                    .OrderBy(p => p.PrecioVenta)
+                    .Take(10)
+                    .ToList();
+                foreach (var planta in query)
+                {
+                    Console.WriteLine($"{planta.Descripcion} {planta.PrecioVenta}");
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+
+        private static void EstadisticasPorTipoEnvaseMasDe30()
+        {
+            using (var context = new ViveroDbContext())
+            {
+                var grupo = context.Plantas.Include(p => p.TipoDeEnvase.Descripcion)
+                    .GroupBy(g => g.TipoDeEnvase.Descripcion)
+                    .Where(g => g.Count() > 30)
+                    .Select(s => new
+                    {
+                        Envase = s.Key,
+                        Cantidad = s.Count()
+                    })
+                    .ToList();
+                foreach (var g in grupo)
+                {
+                    Console.WriteLine($"{g.Envase} {g.Cantidad}");
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void EstadisticaPorTipoEnvase()
+        {
+            using (var context = new ViveroDbContext())
+            {
+                var grupo = context.Plantas.Include(p => p.TipoDeEnvase)
+                    .GroupBy(g => g.TipoDeEnvase.Descripcion)
+                    .OrderBy(g => g.Key)
+                    .Select(s => new
+                    {
+                        Envase = s.Key,
+                        Cantidad = s.Count(),
+                        MayorPrecio = s.Max(se => se.PrecioVenta),
+                        MenorPrecio = s.Min(se => se.PrecioVenta)
+                    }).ToList();
+                foreach (var gr in grupo)
+                {
+                    Console.WriteLine($"{gr.Envase} {gr.Cantidad} {gr.MenorPrecio} {gr.MayorPrecio}");
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void AgruparPorTipo()
+        {
+            using (var context = new ViveroDbContext())
+            {
+                var grupo = context.Plantas.Include(p => p.TipoDePlanta)
+                    .GroupBy(g => g.TipoDePlanta.Descripcion).ToList();
+                foreach (var gr in grupo)
+                {
+                    Console.WriteLine($"Tipo de Planta: {gr.Key}");
+                    foreach (var p in gr)
+                    {
+                        Console.WriteLine($"{p.Descripcion} {p.PrecioVenta}");
+                    }
+                    Console.WriteLine($"Cantidad: {gr.Count()}");
+                    Console.Write("Presione enter para continuar");
+                    Console.ReadLine();
+                }
+            }
+            Console.ReadLine();
         }
 
         private static void AgregarNuevaPlanta()
@@ -97,9 +320,9 @@ namespace ViveroEF2022.Consola
                 TipoDePlantaId = nuevoTipo
             };
 
-            using (var context=new ViveroDbContext())
+            using (var context = new ViveroDbContext())
             {
-                if (!context.Plantas.Any(p=>p.Descripcion==nuevaPlanta.Descripcion))
+                if (!context.Plantas.Any(p => p.Descripcion == nuevaPlanta.Descripcion))
                 {
                     context.Plantas.Add(nuevaPlanta);
                     context.SaveChanges();
@@ -151,11 +374,11 @@ namespace ViveroEF2022.Consola
             {
                 Console.Write("Seleccione un tipo de envase:");
                 tipoSeleccionado = int.Parse(Console.ReadLine());
-                using (var context=new ViveroDbContext())
+                using (var context = new ViveroDbContext())
                 {
                     var tipoEnDb = context.TiposDeEnvases
                         .SingleOrDefault(tp => tp.TipoDeEnvaseId == tipoSeleccionado);
-                    if (tipoEnDb==null)
+                    if (tipoEnDb == null)
                     {
                         Console.WriteLine("ERROR al seleccionar el tipo de envase");
                     }
@@ -188,7 +411,7 @@ namespace ViveroEF2022.Consola
         private static void ListarTiposDeEnvases()
         {
             //Console.Clear();
-            using (var context=new ViveroDbContext())
+            using (var context = new ViveroDbContext())
             {
                 var lista = context.TiposDeEnvases.ToList();
                 foreach (var tipoDeEnvase in lista)
@@ -202,7 +425,7 @@ namespace ViveroEF2022.Consola
 
         private static void AgregarTipoDePlanta()
         {
-            using (var context=new ViveroDbContext())
+            using (var context = new ViveroDbContext())
             {
                 Console.Write("Ingrese el tipo de planta:");
                 var tipoDePlantaIngresada = Console.ReadLine();
@@ -210,13 +433,13 @@ namespace ViveroEF2022.Consola
                 {
                     Descripcion = tipoDePlantaIngresada
                 };
-                if (!context.TiposDePlantas.Any(tp=>tp.Descripcion==nuevoTipoPlanta.Descripcion))
+                if (!context.TiposDePlantas.Any(tp => tp.Descripcion == nuevoTipoPlanta.Descripcion))
                 {
 
                     context.TiposDePlantas.Add(nuevoTipoPlanta);
                     context.SaveChanges();
                     Console.WriteLine("Registro agregado");
-                   
+
 
                 }
                 else
@@ -231,7 +454,7 @@ namespace ViveroEF2022.Consola
         {
             Console.Write("Ingrese la cantidad de registros por página:");
             var cantidadPorPagina = int.Parse(Console.ReadLine());
-            using (var context=new ViveroDbContext())
+            using (var context = new ViveroDbContext())
             {
                 var paginas = CalcularPaginas(cantidadPorPagina, context.Plantas.Count());
                 for (int pagina = 0; pagina < paginas; pagina++)
@@ -239,8 +462,8 @@ namespace ViveroEF2022.Consola
                     Console.WriteLine($"Página: {pagina}");
                     Console.WriteLine("------------Detalle ------");
                     var detallePlantas = context.Plantas
-                        .OrderBy(p=>p.Descripcion)
-                        .Skip(pagina*cantidadPorPagina)
+                        .OrderBy(p => p.Descripcion)
+                        .Skip(pagina * cantidadPorPagina)
                         .Take(cantidadPorPagina).ToList();
                     foreach (var planta in detallePlantas)
                     {
@@ -254,11 +477,11 @@ namespace ViveroEF2022.Consola
 
         private static int CalcularPaginas(int cantidadPorPagina, int cantidadPlantas)
         {
-            if (cantidadPorPagina>cantidadPlantas)
+            if (cantidadPorPagina > cantidadPlantas)
             {
                 return 1;
             }
-            else if(cantidadPlantas % cantidadPorPagina>0)
+            else if (cantidadPlantas % cantidadPorPagina > 0)
             {
                 return cantidadPlantas / cantidadPorPagina + 1;
             }
@@ -272,11 +495,11 @@ namespace ViveroEF2022.Consola
         {
             Console.Write("Ingrese un precio:");
             var precio = decimal.Parse(Console.ReadLine());
-            using (var context=new ViveroDbContext())
+            using (var context = new ViveroDbContext())
             {
                 //var lista = context.Plantas.Where(p => p.PrecioVenta < precio).Take(10).ToList();
                 var lista = context.Plantas.Where(p => p.PrecioVenta < precio)
-                    .OrderBy(p=>p.PrecioVenta)
+                    .OrderBy(p => p.PrecioVenta)
                     .Take(10).ToList();
 
                 foreach (var planta in lista)
@@ -290,7 +513,7 @@ namespace ViveroEF2022.Consola
 
         private static void ProyectarEnObjetoAnonimo()
         {
-            using (var context=new ViveroDbContext())
+            using (var context = new ViveroDbContext())
             {
                 var lista = context.Plantas
                     .Include(p => p.TipoDePlanta)
@@ -335,7 +558,7 @@ namespace ViveroEF2022.Consola
                     {
                         context.TiposDeEnvases.Remove(tipoEnvaseInDb);
                         context.SaveChanges();
-                        
+
                         Console.WriteLine("Registro borrado");
                         break;
                     }
@@ -391,7 +614,7 @@ namespace ViveroEF2022.Consola
 
             }
 
-            
+
         }
 
         private static void AgregarTipoDeEnvase()
